d4abf51e3f5d8e02afe33b1c834bae70fa7a4030
Samantha Sedran <ssedran@bayviewglen.ca>
2021-09-09 09:30:03 -0400

    Enter

---
+++ Unit 1 - Introduction to CS/src/Week1/ExampleOne.java
@@ -0,0 +1,14 @@
+package Week1;
+
+// Every file will be its own class FOR NOW (you cna have a file with multiple classes, will do later)
+public class ExampleOne {
+
+    public static void main(String[] args) { //wherejavastartsrunning the code from it looks for a function called main
+        //The main method (function) Let's Java know where to start.
+        System.out.println("Welcome to ICS 3U - AP");
+        //System.out.println writes to the console, whatever's in the brackets goes to the console
+        System.out.println("Welcome to ICS 3U - AP");
+        System.out.println("Welcome to ICS 3U - AP");
+        System.out.println("Welcome to ICS 3U - AP");
+    }
+}
