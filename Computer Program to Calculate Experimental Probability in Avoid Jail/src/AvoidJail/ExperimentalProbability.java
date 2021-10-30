c6b5640b2317ca587a2ff8c6a70b8b01587cd035
Samantha Sedran <ssedran@bayviewglen.ca>
2021-10-30 10:31:21 -0400

    Enter

--- Computer Program to Calculate Experimental Probability in Avoid Jail/src/AvoidJail/ExperimentalProbability.java
+++ Computer Program to Calculate Experimental Probability in Avoid Jail/src/AvoidJail/ExperimentalProbability.java
@@ -5,7 +5,7 @@ public class ExperimentalProbability {
         int spun = 0;
         int die1 = 0;
         int die2 = 0;
-        int trials = 50;
+        int trials = 1000;
         int onedie = 0;
         int twodie = 0;
         int diffnum = 0;
@@ -14,11 +14,10 @@ public class ExperimentalProbability {
             spun = rollspin(spun);
             die1 = rollspin(spun);
             die2 = rollspin(spun);
-            System.out.println(spun+" "+die1+" "+die2);
-            if(spun==die1||spun==die2){
-                onedie++;
-            }else if(spun==die1&&spun==die2){
+            if((spun==die1)&&(spun==die2)){
                 twodie++;
+            }else if((spun==die1)||(spun==die2)){
+                onedie++;
             }else{
                 diffnum++;
             }
