4668d08df4cb9faa928fef4a363f17bf5a4676db
Samantha Sedran <ssedran@bayviewglen.ca>
2021-10-31 22:52:06 -0400

    Crazy Eights  - Final

--- Unit 4 - Loops/src/Week7/CrazyEights.java
+++ Unit 4 - Loops/src/Week7/CrazyEights.java
@@ -25,7 +25,11 @@ public class CrazyEights {
          playNow = play(in);
       }
     }
-
+/**
+ * 
+ * @param in - Scanner
+ * @return - if the player wants ton play again as a boolean value
+ */
     private static boolean playAgain(Scanner in) {
       boolean validInput = false;
 
@@ -43,6 +47,11 @@ public class CrazyEights {
 
       return false;
    }
+   /**
+    * Plays a game
+    * @param in - scanner
+    * @return - if player would like to play again as a boolean value
+    */
 
     private static boolean play(Scanner in) {
       int pPoints = 0;
@@ -79,12 +88,12 @@ public class CrazyEights {
           firstCard = round.substring(round.indexOf("_")+1);
           roundDone = roundOver(pHand, c1Hand, c2Hand);
           }
-          pPoints = getPoints(pHand);
-          c1Points = getPoints(c1Hand);
-          c2Points = getPoints(c2Hand);
-          System.out.print("Player has "+pPoints+"points");
-          System.out.print("Computer player 1 has "+c1Points+"points");
-          System.out.print("Computer player 2 has "+c2Points+"points");
+          pPoints = getPoints(pHand, pPoints);
+          c1Points = getPoints(c1Hand, c1Points);
+          c2Points = getPoints(c2Hand, c2Points);
+          System.out.println("Player has "+pPoints+" points");
+          System.out.println("Computer player 1 has "+c1Points+" points");
+          System.out.println("Computer player 2 has "+c2Points+" points");
           gameOver = gameDone(pPoints, c1Points, c2Points);
        }
        winner(pPoints, c1Points, c2Points);
@@ -96,7 +105,17 @@ public class CrazyEights {
        }
    }
 
+   /**
+    * deals cards to each player
+    * @param pHand - player's hand
+    * @param c1Hand - computer player 1's hand
+    * @param c2Hand - computer player 2's hand
+    * @return
+    */
    private static String deal(String pHand, String c1Hand, String c2Hand) {
+      pHand = "";
+      c1Hand = "";
+      c2Hand = "";
       for(int i = 0; i<MAX_CARDS; i++){
          pHand = getCard() + " " + pHand;
          c1Hand = getCard() + " " + c1Hand;
@@ -105,62 +124,93 @@ public class CrazyEights {
      return pHand +"-"+ c1Hand +"*"+ c2Hand;
    }
 
+   /**
+    * deterines who the winner is
+    * @param pPoints - player's points
+    * @param c1Points - computer player 1's points
+    * @param c2Points - computer player 2's points
+    */
 
    private static void winner(int pPoints, int c1Points, int c2Points) {
-      System.out.print("Player has "+pPoints+"points");
-      System.out.print("Computer player 1 has "+c1Points+"points");
-      System.out.print("Computer player 2 has "+c2Points+"points");
       if(pPoints<c1Points && pPoints<c2Points){
-         System.out.print("You won!!!!");
+         System.out.println("You won!!!!");
       }else if(c1Points<pPoints && c1Points<c2Points){
-         System.out.print("Computer Player 1 won!!!!");
+         System.out.println("Computer Player 1 won!!!!");
       }else if(c2Points<pPoints && c2Points<c1Points){
-         System.out.print("Computer Player 2 won!!!!");
+         System.out.println("Computer Player 2 won!!!!");
       }else if(pPoints==c1Points && pPoints<c2Points){
-         System.out.print("Tie between Player and Computer Player 1!");
+         System.out.println("Tie between Player and Computer Player 1!");
       }else if(c1Points==c2Points && c1Points<pPoints){
-         System.out.print("Tie between Computer Player 1 and Computer Player 2!");
+         System.out.println("Tie between Computer Player 1 and Computer Player 2!");
       }else if(pPoints==c2Points && pPoints<c1Points){
-         System.out.print("Tie between Player and Computer Player 2!");
+         System.out.println("Tie between Player and Computer Player 2!");
       }
    }
 
-
+/**
+ * determines if the game is over or not (whether at least one player has 100 points or more)
+ * @param pPoints - player's points
+ * @param c1Points - computer player 1's points
+ * @param c2Points - computer player 2's points
+ * @return - whether the game is over or not
+ */
    private static boolean gameDone(int pPoints, int c1Points, int c2Points) {
-       if(pPoints==100 || c1Points==100 || c2Points==100){
+       if(pPoints>=100 || c1Points>=100 || c2Points>=100){
          return true;
        }else{
           return false;
        }
    }
+/**
+ * Determines the number of total points a player has over all rounds they've played
+ * @param hand - player's hand
+ * @param points - total points from previous rounds (if this is the first round, this value equals zero)
+ * @return - the total points
+ */
 
-
-   private static int getPoints(String hand) {
+   private static int getPoints(String hand, int points) {
       int totalPoints = 0;
       for(int i = 0; i<hand.length(); i+=3){
          if(hand.substring(i, i+1).equals("8")){
             totalPoints += 50;
-         }else if(Integer.parseInt(hand.substring(i, i+1))>1 && Integer.parseInt(hand.substring(i, i+1))<10){
-            totalPoints += Integer.parseInt(hand.substring(i, i+1));
          }else if(hand.substring(i, i+1).equals("A")){
             totalPoints += 1;
          }else if(hand.substring(i, i+1).equals("T")||hand.substring(i, i+1).equals("J")||hand.substring(i, i+1).equals("Q")||hand.substring(i, i+1).equals("K")){
             totalPoints += 10;
+         }else if(Integer.parseInt(hand.substring(i, i+1))>1 && Integer.parseInt(hand.substring(i, i+1))<10){
+            totalPoints += Integer.parseInt(hand.substring(i, i+1));
          }         
        }
+       totalPoints += points;
       return totalPoints;
    }
-
+/**
+ * Determines if the round is over or not by checking if any player has cards left in their hand
+ * @param pHand - player's hand
+ * @param c1Hand - computer player 1's hand
+ * @param c2Hand - computer player 2's hand
+ * @return - whther the round is over or not as a boolean value
+ */
 
    private static boolean roundOver(String pHand, String c1Hand, String c2Hand) {
-       if((pHand=="")||(c1Hand=="")||(c2Hand=="")){
+       if(pHand.equals("")||c1Hand.equals("")||c2Hand.equals("")){
           System.out.println("-----------------------------------------------");
           return true;
-       }else{
-            System.out.println("-----------------------------------------------");
+       }else {
+         System.out.println("-----------------------------------------------");
           return false;
        }
    }
+
+   /**
+    * Plays a round of the game where each player plays one card
+    * @param pHand - player's hand
+    * @param c1Hand - computer player 1's hand
+    * @param c2Hand - computer player 2's hand
+    * @param firstCard - face up card
+    * @param in - scanner
+    * @return - player's, computer 1's, computer 2's hands and the face up card all attached together and seperated with the characters "-", "*", and "_"
+    */
 
 
    private static String round(String pHand, String c1Hand, String c2Hand, String firstCard, Scanner in) {
@@ -169,7 +219,7 @@ public class CrazyEights {
       System.out.println("Computer Player 2's Hand: "+ifTen(c2Hand));
       System.out.println();
 
-      System.out.println("The 1st card drawn from the stock pile is: "+ifTen(firstCard));
+      System.out.println("The face up card is: "+ifTen(firstCard));
       System.out.println();
 
       String beforePlayerFirstCard = firstCard;
@@ -203,6 +253,14 @@ public class CrazyEights {
        return pHand +"-"+ c1Hand +"*"+ c2Hand +"_"+ firstCard;
    }
 
+   /**
+    * This method first checks if the card the player wants to play is an 8.
+    * If the player plays an eight, this method asks it what suit they want to play.
+    * if not, the player's card is just returned
+    * @param card - the card the player wants to play
+    * @param in - scanner
+    * @return - An "8" with the suit that the player wants to play (represented as "H","C","S", or "D") attached to it
+    */
 
    private static String playerEight(String card, Scanner in) {
       if(card.substring(0,1).equals("8")){
@@ -225,6 +283,14 @@ public class CrazyEights {
    }
       return card;
    }
+   /**
+    * If the computer can play, this method plays the card the computer can play
+    * If the computer cannot, it draws another card for the computer and keeps drawing cards until the computer can play, or has picked up 5 cards
+    * @param hand - computer's hand
+    * @param firstCard - face up card
+    * @param cNum - the computer player number (1 or 2)
+    * @return - the computer's hand with the "-" character next to it and the the face up card all in one string
+    */
 
    private static String computerPlay(String hand, String firstCard, int cNum) {
       String card  = computerMove(hand, firstCard);
@@ -236,7 +302,7 @@ public class CrazyEights {
             if(c1numPickedUp<5){
                c1numPickedUp++;
                String compickup = getCard();
-               System.out.println("Computer cannot play with the cards it has. So it picks up: "+ifTen(compickup));
+               System.out.println("Computer "+cNum+" cannot play with the cards it has. So it picks up: "+ifTen(compickup));
                hand += compickup + " ";
                card = computerMove(hand, firstCard);
                if(card.indexOf("-drawagain")>=0){
@@ -246,7 +312,6 @@ public class CrazyEights {
                   computerPlay = true;
                }
             }else{
-               hand = card.substring(0,card.indexOf("-drawagain"));
                System.out.println("Computer "+cNum+" cannot play. Next player goes.");
                return hand+"-"+firstCard;
             }
@@ -258,11 +323,34 @@ public class CrazyEights {
       }else if(hand.indexOf(card)==hand.length()-card.length()-1){
          hand = hand.substring(0, hand.indexOf(card));
       }else{
-         hand = hand.substring(0, hand.indexOf(card)-1)+hand.substring(hand.indexOf(card)+card.length());
+         hand = hand.substring(0, hand.indexOf(card))+hand.substring(hand.indexOf(card)+card.length()+1);
+      }
+      if(firstCard.substring(0,1).equals("8")){
+         String suit = getSuit();
+         String printsuit = "";
+         if(suit.equals("H")){
+            printsuit = "hearts.";
+         }else if(suit.equals("C")){
+            printsuit = "clubs.";
+         }else if(suit.equals("S")){
+            printsuit = "spades.";
+         }else {
+            printsuit = "diamonds.";
+         }
+         System.out.println("Computer Player "+cNum+" has played an eight. The computer chooses "+printsuit);
+         firstCard = "8"+suit;
       }
       System.out.println("Computer "+cNum+" plays: "+ifTen(firstCard));
       return hand+"-"+firstCard;
    }
+
+   /**
+    * 10 uses one more character than any other number from A to K. So in the code, "T" represents 10. 
+    * This method checks if there is a 10 in the string entered and changes the 10 to a T so the string can be used in the code
+    * This method is used when the player inputs the card they want to play. If they want to play a ten, it changes "10" to "T"
+    * @param str - any string value entered in Ex. the player's hand, the face up card, etc.
+    * @return - the string with all the "10"'s from the string replaced with T's
+    */
 
    private static String reverseTen(String str) {
       String answer = "";
@@ -282,6 +370,13 @@ public class CrazyEights {
       return answer;
    }
 
+   /**
+    * 10 uses one more character than any other number from A to K. So in the code, "T" represents 10. 
+    * This method checks if there is a T in the string entered and changes the T to a 10 so the string can be printed
+    * @param str - any string value entered in Ex. the player's hand, the face up card, etc.
+    * @return - the string with all the "T"'s from the string replaced with 10's
+    */
+
    private static String ifTen(String str) {
       String answer = "";
       if(str.indexOf("T")>=0){
@@ -297,6 +392,14 @@ public class CrazyEights {
       }
       return answer;
    }
+
+   /**
+    * This method plays the card the player selected on top of the face up card if they can play, If they cannot play, the computer picks up a card for them and adds it to their hand and checks if they can play now that they have a new card
+    * @param pHand - the player's hand
+    * @param in - scanner
+    * @param firstCard - the current face up card
+    * @return - the player's hand then the character "-" thent he face up card
+    */
 
    private static String playerPlay(String pHand, Scanner in, String firstCard) {
       boolean play = false;
@@ -322,6 +425,12 @@ public class CrazyEights {
       return pHand+"-"+firstCard;
 }
 
+/**
+ * Determines which card the computer plays, and if it can play
+ * @param hand - the computer's hand
+ * @param firstCard - the current face up card
+ * @return - if the computer can play, the card it will play, if it cannot then the computer's hand with the phrase "-drawagain" attached to it
+ */
    private static String computerMove(String hand, String firstCard) {
       boolean play = false;
       String answer = "";
@@ -345,7 +454,7 @@ public class CrazyEights {
          if(!play){
             for(int i = 0; i<hand.length(); i++){
                if(hand.substring(i, i+1).equals("8")){
-                  answer = "8"+getSuit();
+                  answer = hand.substring(i, i+2);
                   play = true;
                   i=hand.length();
                }
@@ -358,6 +467,13 @@ public class CrazyEights {
       return answer;
 
    }
+
+   /**
+    * This method determines whether the player can play or not
+    * @param hand - player's hand
+    * @param card - face up card
+    * @return - if the player can play or not
+    */
 
    private static boolean canPlay(String hand, String card) {
       for(int i = 0; i<hand.length(); i++){
@@ -372,7 +488,15 @@ public class CrazyEights {
       return false;
     }
 
-    private static String playerMove(Scanner in, String hand, String firstCard) {
+    /**
+     * This method asks the user what card they would like to play from their deck, if they can play. 
+     * @param in - scanner
+     * @param hand - The player's current hand before selecting a card
+     * @param firstCard - the current face up card
+     * @return - the card the player selects to play
+     */
+
+    private static String playerMove(Scanner in, String hand, String firstCard) { 
       boolean play = false;
       String answer = "";
       while(!play){
@@ -403,12 +527,22 @@ public class CrazyEights {
       return answer.substring(0,2);
     }
 
-    private static String getCard() {
+    /**
+     * creates a card by calling the methods getSuit and getCard
+     * @return - the card
+     */
+
+    private static String getCard() { 
         String card = getFace() + getSuit();
         return card;
      }
+
+     /**
+      * gets suit for a card
+      * @return - the suit for the card
+      */
   
-     private static String getSuit() {
+     private static String getSuit() { 
         int suit = (int) (Math.random() * 4);
         if (suit == 0){
            return HEARTS;
@@ -420,8 +554,13 @@ public class CrazyEights {
            return DIAMONDS;
         }
      }
+
+     /**
+      * gets face for a card
+      * @return - returns the face
+      */
   
-     private static String getFace() {
+     private static String getFace() { 
         int face = (int) (Math.random() * 13) + 1;
         if (face > 1 && face < 10)
            return "" + face;
